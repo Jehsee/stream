8d0424b9e3ba1fb196734c9bba3dcf853dd34f14
Jesse <jessehwang86@yahoo.com>
2016-10-09 14:49:06 -0400

    displayed messages in chatroom#show

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomsController < ApplicationController
   def show
+    @messages = Message.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'chatrooms/show'
+  root to: 'chatrooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
